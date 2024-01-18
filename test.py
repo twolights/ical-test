c7c2f62a1d02eea9334074f7df1ebc0ce712d33f
Evan Chen <twolights@gmail.com>
2024-01-18 09:20:25 +0800

    Temp commit

--- test.py
+++ test.py
@@ -3,14 +3,18 @@ from datetime import datetime
 import pytz
 from icalendar import Calendar, Event
 
+cal = None
+
+with open('test.ics', 'rb') as f:
+    cal = Calendar.from_ical(f.read())
+    
 # Add subcomponents
 event = Event()
-event.add('name', 'Meeting #001')
-event.add('description', 'This is a meeting')
-event.add('dtstart', datetime(2024, 1, 25, 8, 0, 0, tzinfo=pytz.timezone('Asia/Taipei')))
-event.add('dtend', datetime(2024, 1, 25, 10, 0, 0, tzinfo=pytz.timezone('Asia/Taipei')))
+event.add('name', 'Meeting 002')
+event.add('description', 'This is aother meeting')
+event.add('dtstart', datetime(2024, 1, 27, 8, 0, 0, tzinfo=pytz.timezone('Asia/Taipei')))
+event.add('dtend', datetime(2024, 1, 27, 10, 0, 0, tzinfo=pytz.timezone('Asia/Taipei')))
 
-cal = Calendar()
 cal.add_component(event)
 
 with open('test.ics', 'wb') as f:
